4de911d2baf6b19b9cad4e924ca2f7be5f1be3dd
Anna Tsvitkova <anna.tsvitkova@outright.digital>
2020-04-13 11:00:47 +0300

    Create reviews-section

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.slider-rooms').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        appendArrows: $('.scroll'),
+        prevArrow: '#rooms-scroll-left',
+        nextArrow: '#rooms-scroll-right',
+    });
+});
+
+$(document).ready(function(){
+    $('.slider-reviews').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        appendArrows: $('.scroll'),
+        prevArrow: '#scroll-left',
+        nextArrow: '#scroll-right',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
