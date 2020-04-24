f121c17a1cb620920a847525693dfb382b1aa8a7
Anna Tsvitkova <anna.tsvitkova@outright.digital>
2020-04-24 17:56:48 +0300

    Fix bugs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,53 +1,54 @@
-window.onload = function() {
-        $(".navbar").on("click","a[href*='#']", function (event) {
-            event.preventDefault();
-            const id  = $(this).attr('href'),
-                top = $(id).offset().top;
-            $('body,html').animate({scrollTop: top}, 1500);
-        });
+$(window).on('load', () => {
+    $(".navbar").on("click","a", function (event) {
+        event.preventDefault();
+        const id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 
-        const burgerMenu = document.querySelector('.burger-menu');
-        const burgerIcon = document.querySelector('.menu-icon'); 
-        const menuItem = document.querySelector('.menu-item');
-        const body = document.querySelector('body');
 
-        burgerMenu.onclick = function () {
-            menuItem.classList.toggle('menu-active');
-            burgerIcon.classList.toggle('burger-active')
-            body.classList.toggle('body-hidden')
-        };
+    const burgerIcon = document.querySelector('.menu-icon'); 
+    const menuItem = document.querySelector('.menu-item');
+    const body = document.querySelector('body');
 
-        $('.slider-rooms').slick({
-            dots: true,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            appendDots: $('.slick-dots'),
-            prevArrow: '#rooms-scroll-left',
-            nextArrow: '#rooms-scroll-right',
-        });
+    $(".burger-menu").on("click", function (event) {
+        event.preventDefault();
+        menuItem.classList.toggle('menu-active');
+        burgerIcon.classList.toggle('burger-active')
+        body.classList.toggle('body-hidden')
+    });
 
-        $('.slider-reviews').slick({
-            dots: true,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 1,
-            variableWidth: true,
-            appendDots: $('.reviews-slick-dots'),
-            prevArrow: '#scroll-left',
-            nextArrow: '#scroll-right',
-        });
+    $('.slider-rooms').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        appendDots: $('.slick-dots'),
+        prevArrow: '#rooms-scroll-left',
+        nextArrow: '#rooms-scroll-right',
+    });
 
-        const directionsDisplay = new google.maps.DirectionsRenderer;
-        const map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 17,
-            center: {lat: 59.9387942, lng: 30.3230833}
-        });
-        directionsDisplay.setMap(map);
-    
-        new google.maps.Marker({
-            position: {lat: 59.9387942, lng: 30.3230833},
-            map: map,
-            icon: 'assets/images/marker.svg',
-        });
-};
+    $('.slider-reviews').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        variableWidth: true,
+        appendDots: $('.reviews-slick-dots'),
+        prevArrow: '#scroll-left',
+        nextArrow: '#scroll-right',
+    });
+
+    const directionsDisplay = new google.maps.DirectionsRenderer;
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: {lat: 59.9387942, lng: 30.3230833}
+    });
+    directionsDisplay.setMap(map);
+
+    new google.maps.Marker({
+        position: {lat: 59.9387942, lng: 30.3230833},
+        map: map,
+        icon: 'assets/images/marker.svg',
+    });
+});
